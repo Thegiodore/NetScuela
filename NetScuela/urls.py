a31f0beacee109b64bba6d2b50ceb712f075a7d6
IvanovichPushkin <mgl3185473@gmail.com>
2023-09-23 10:51:42 +0800

    Thegiodore/NetScuela

--- NetScuela/urls.py
+++ NetScuela/urls.py
@@ -27,4 +27,6 @@ urlpatterns = [
     path('', home, name='home'),
 ]
 
-
+if settings.DEBUG:
+    urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+    urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
